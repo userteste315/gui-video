3a256992262c39d14e1b855f25646d47a2626471
rodrigo_svieira <rodrigo_svieira@terceiros.sicredi.com.br>
2023-06-06 15:26:35 -0300

    edit workflow + test

--- src/test/java/br/com/guivideo/GuiVideoApplicationTests.java
+++ src/test/java/br/com/guivideo/GuiVideoApplicationTests.java
@@ -3,11 +3,14 @@ package br.com.guivideo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class GuiVideoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(1).isEqualTo(1);
 	}
 
 }
